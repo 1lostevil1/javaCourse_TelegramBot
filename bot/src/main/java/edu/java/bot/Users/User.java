6429f85de097844518040f318690513a059a3fbf
1lostevil1 <ki-04@yandex.ru>
2024-02-18 15:52:09 +0300

    raw bot - checkstyle

--- bot/src/main/java/edu/java/bot/Users/User.java
+++ bot/src/main/java/edu/java/bot/Users/User.java
@@ -2,4 +2,5 @@ package edu.java.bot.Users;
 
 import java.util.List;
 
-public record User (String name, Long id, List<String> urls){};
+public record User(String name, Long id, List<String> urls) {
+};
