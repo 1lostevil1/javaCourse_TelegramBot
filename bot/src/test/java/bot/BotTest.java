0837a82d287f69704ef32674947225497dde8562
1lostevil1 <ki-04@yandex.ru>
2024-02-19 00:43:23 +0300

    raw bot - bad tests

--- bot/src/test/java/bot/BotTest.java
+++ bot/src/test/java/bot/BotTest.java
@@ -2,36 +2,37 @@
 
 package bot;
 
-import com.pengrad.telegrambot.TelegramException;
+import com.pengrad.telegrambot.model.Chat;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
-import edu.java.bot.Controller.TelegramBot;
-import org.junit.jupiter.api.BeforeEach;
+import com.pengrad.telegrambot.request.SendMessage;
+import edu.java.bot.Command.Command;
+import edu.java.bot.Command.Start;
+import edu.java.bot.Users.Users;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
 public class BotTest {
 
-    private Update update;
-    private TelegramBot bot = new TelegramBot("6863584041:AAEa0ZBYr8SKB0_X8ckJvnT7s5qJqjXPLUE");
-
-
-       @BeforeEach
-        public void init() {
-            update = Mockito.mock(Update.class);
-            Mockito.when(update.message()).thenReturn(Mockito.mock(Message.class));
-            Mockito.when(update.message().text()).thenReturn("/start");
-
-        }
-
-
-        public void shouldProperlySendMessage() throws TelegramException {
-
-            bot.run();
-        }
+    @Test
+    @DisplayName("/start check")
+    void test1() {
+        //given
+        Users base = new Users();
+        Command startCommand = new Start();
+        Update update = mock(Update.class);
+        Message message = mock(Message.class);
+        //when
+        when(update.message()).thenReturn(message);
+        when(message.text()).thenReturn("/start");
+        when(update.message().chat()).thenReturn(new Chat());
+        SendMessage response = startCommand.apply(update, base);
+        //then
+        assertThat("Добро пожаловать, заюш").isEqualTo(response.getParameters().get("text"));
+    }
 }
 
 
