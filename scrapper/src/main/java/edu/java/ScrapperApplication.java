f828508b56644aeb725bdc4641097a92efdc745e
1lostevil1 <ki-04@yandex.ru>
2024-03-17 23:21:47 +0300

    hw5

--- scrapper/src/main/java/edu/java/ScrapperApplication.java
+++ scrapper/src/main/java/edu/java/ScrapperApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @SpringBootApplication
 @EnableConfigurationProperties(ApplicationConfig.class)
 public class ScrapperApplication {
+    int x;
     public static void main(String[] args) {
         SpringApplication.run(ScrapperApplication.class, args);
     }
